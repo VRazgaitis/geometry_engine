ecf08527f9cdf4b1fff40bd0a252a40212209328
VRazgaitis <vaidas.razgaitis@gmail.com>
2024-04-25 13:09:03 -0500

    setup file

---
+++ setup_and_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create the virtual environment
+python -m venv myenv
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Set environment variables
+export FLASK_APP=app
+export FLASK_ENV=development
+
+# Run the Flask application
+flask run --reload
